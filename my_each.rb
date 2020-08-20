c7013dd55153bcf0eca5f7237c2d3a2844c3c5b0
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-20 08:08:22 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each (array)# put argument(s) here
   # code here
+  i = 0
+  while array.length > i 
+      yield(array[i])
+      i = i + 1
+  end
+  array
 end
